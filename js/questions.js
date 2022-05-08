01c7cca02a22f6b9c3a542381515613b8b0a7352
shankarvalar <100361489+shankarvalar@users.noreply.github.com>
2022-05-08 09:49:52 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the favourite actor of shankar?",
+    answer: "dhanush",
+    options: [
+      "suriya",
+      "dhanush",
+      "rajini",
+      "ajith"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In Which college shankar study?",
+    answer: "Vellamal Engineering College",
+    options: [
+      "RMK College",
+      "SRM college",
+      "Presidency college",
+      "Vellamal Engineering College"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the Favourite food of shankar?",
+    answer: "All of the above",
+    options: [
+      "All of the above",
+      "biriyani",
+      "IDLE ",
+      "mutton rice"
+    ]
+  },
+    {
+    numb: 4,
+    question: "shankar is the good person or not",
+    answer: "May Be!",
+    options: [
+      "Yes!",
+      "No",
+      "Very Good Person",
+      "May Be!"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In which transport vehicle shankar comes?",
+    answer: "Government Bus",
+    options: [
+      "Government Bus",
+      "College Bus",
+      "None of the above",
+      "two Wheeler"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
